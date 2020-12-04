0ab2725eb4f6e41a5cb7e95dec0bd4981bbdd614
neevparikh <neev_parikh@brown.edu>
2020-12-04 18:55:16 -0500

    Fixed bugs

--- data.py
+++ data.py
@@ -13,17 +13,23 @@ torch.multiprocessing.set_sharing_strategy('file_descriptor')
 splits = [0.5, 0.3, 0.2]
 
 
-def get_data(name):
+def get_data(name, dtype):
     data = np.load(os.path.join(DATA_DIR, name), allow_pickle=True)
     unique_labels, labels = np.unique(data['labels'], return_inverse=True)
     spects = np.expand_dims(data['spects'], axis=1)
     print(labels.shape, labels[:10])
     print(spects.shape, spects[:10])
-    return labels, spects.astype(np.float16)
+    return labels, spects.astype(dtype)
 
 
 class FMA(pl.LightningDataModule):
     shape = (128, 640)
+
+    def __init__(self, args):
+        self.args = args
+        self.batchsize = args.batchsize
+        self.num_data_workers = args.data_workers
+        self.dtype = np.float16 if args.num_gpus > 0 else np.float32
 
     def train_dataloader(self):
         print("Number of training samples", len(self.train))
@@ -39,12 +45,10 @@ class FMA(pl.LightningDataModule):
 
 
 class FMA_Large(FMA):
-    def setup(self, args, stage=None):
-        self.labels, self.spects = get_data('fma_large_combined.npz')
+    def setup(self, stage=None):
+        self.labels, self.spects = get_data('fma_large_combined.npz', self.dtype)
         self.labels = torch.from_numpy(self.labels)
         self.num_labels = 161
-        self.batchsize = args.batchsize
-        self.num_data_workers = args.data_workers
         self.spects = torch.from_numpy(self.spects)
         self.dataset = TensorDataset(self.spects, self.labels)
         split = [math.floor(len(self.dataset) * x) for x in splits]
@@ -54,12 +58,10 @@ class FMA_Large(FMA):
 
 class FMA_Small(FMA):
     def setup(self, args, stage=None):
-        self.labels, self.spects = get_data('fma_small_combined.npz')
+        self.labels, self.spects = get_data('fma_small_combined.npz', self.dtype)
         self.labels = torch.from_numpy(self.labels)
         self.spects = torch.from_numpy(self.spects)
         self.num_labels = 8
-        self.batchsize = args.batchsize
-        self.num_data_workers = args.data_workers
         self.dataset = TensorDataset(self.spects, self.labels)
         split = [math.floor(len(self.dataset) * x) for x in splits]
         split[-1] += len(self.dataset) - sum(split)
@@ -67,12 +69,16 @@ class FMA_Small(FMA):
 
 
 class GTZAN(pl.LightningDataModule):
-    def setup(self, args, stage=None):
-        self.shape = (128, 660)
-        self.num_labels = 10
+    def __init__(self, args):
+        self.args = args
         self.batchsize = args.batchsize
         self.num_data_workers = args.data_workers
-        self.labels, self.spects = get_data('gtzan.npz')
+        self.dtype = np.float16 if args.num_gpus > 0 else np.float32
+
+    def setup(self, stage=None):
+        self.shape = (128, 660)
+        self.num_labels = 10
+        self.labels, self.spects = get_data('gtzan.npz', self.dtype)
         self.labels = torch.from_numpy(self.labels)
         self.spects = torch.from_numpy(self.spects)
         self.dataset = TensorDataset(self.spects, self.labels)
--- train.py
+++ train.py
@@ -7,19 +7,19 @@ from data import FMA_Large, FMA_Small, GTZAN
 args = common_parser.parse_args()
 
 if args.dataset == 'FMA_L':
-    data = FMA_Large()
+    data = FMA_Large(args)
 elif args.dataset == 'FMA_S':
-    data = FMA_Small()
+    data = FMA_Small(args)
 elif args.dataset == 'GTZAN':
-    data = GTZAN()
+    data = GTZAN(args)
 else:
     raise ValueError("Unrecognized dataset")
 data.prepare_data()
-data.setup(args, 'fit')
+data.setup('fit')
 net = GenreNet(args, input_shape=data.shape, num_classes=data.num_labels)
 trainer = pl.Trainer(gpus=args.num_gpus,
-                     precision=16,
-                     accelerator='ddp',
+                     precision=16 if args.num_gpus > 0 else 32,
+                     accelerator='ddp' if args.num_gpus > 0 else None,
                      limit_train_batches=1.0,
                      limit_val_batches=1.0,
                      limit_test_batches=1.0)
